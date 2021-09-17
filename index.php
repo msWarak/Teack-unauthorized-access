f8f57e77ef3cda220bce0b38be25dba2b40b675d
msWarak <mansour.worak@gmail.com>
2021-09-17 17:38:48 +0300

    Updating version to 1.1.5

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  Plugin URI: http://mswarak.com
  Description: Track unauthorized access to your WP website
  Author: msWarak
- Version: 1.1.4
+ Version: 1.1.5
  Author URI: https://mswarak.com
 */
 
